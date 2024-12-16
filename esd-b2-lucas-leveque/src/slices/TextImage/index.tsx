8d989876f998111d2fa401b899ec692f50bb0d31
1u1ucas <lucasleveque17@gmail.com>
2024-12-16 09:16:08 +0100

    --

---
+++ esd-b2-lucas-leveque/src/slices/TextImage/index.tsx
@@ -0,0 +1,34 @@
+import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import style from "./index.module.css";
+
+/**
+ * Props for `TextImage`.
+ */
+export type TextImageProps = SliceComponentProps<Content.TextImageSlice>;
+
+/**
+ * Component for "TextImage" Slices.
+ */
+const TextImage = ({ slice }: TextImageProps): JSX.Element => {
+  const { text, image } = slice.primary;
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className={style.container}
+    >
+      <div>
+        {/* Text */}
+        <PrismicRichText field={text} />
+      </div>
+      <div>
+        {/* Image */}
+        <PrismicNextImage field={image} />
+      </div>
+    </section>
+  );
+};
+
+export default TextImage;
